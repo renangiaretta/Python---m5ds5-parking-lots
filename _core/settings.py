4a62bbd393be7995acd026c69cea9e3797f32826
renangiaretta <renan.giaretta@hotmail.com>
2023-04-28 11:03:57 -0300

    fix: core settings

--- _core/settings.py
+++ _core/settings.py
@@ -12,14 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import ipdb
 import dotenv
 from datetime import timedelta
 import dj_database_url
 from django.core.management.utils import get_random_secret_key
 
-
 dotenv.load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
+# ipdb.set_trace()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -36,10 +36,15 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")
+RAILWAY_STATIC_URL = os.getenv("RAILWAY_STATIC_URL")
+
 
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS += [RENDER_EXTERNAL_HOSTNAME, "0.0.0.0"]
-# ipdb.set_trace()
+
+if RAILWAY_STATIC_URL:
+    ALLOWED_HOSTS += [RAILWAY_STATIC_URL, "0.0.0.0"]
+
 # Application definition
 
 DJANGO_APPS = [
@@ -61,7 +66,9 @@ THIRD_PARTY_APPS = [
     "drf_spectacular",
 ]
 
+
 INSTALLED_APPS = DJANGO_APPS + MY_APPS + THIRD_PARTY_APPS
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -103,20 +110,41 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "USER": os.getenv("POSTGRES_USERNAME"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "NAME": os.getenv("POSTGRES_NAME"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT"),
-    }
+        "NAME": os.getenv("POSTGRES_DB_NAME"),
+        "HOST": os.getenv("POSTGRES_DB_HOST"),
+        "PORT": os.getenv("POSTGRES_DB_PORT"),
+    },
+    "outro_nome": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
 }
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+# postgres://username:password@host:port/database
+"""
+"default": {
+    "ENGINE": "django.db.backends.postgresql",
+    "USER": username,
+    "PASSWORD": password,
+    "NAME": database,
+    "HOST": host,
+    "PORT": port,
+}
+"""
+
+
 if DATABASE_URL:
     db_from_env = dj_database_url.config(
-        default=DATABASE_URL, conn_max_age=500, ssl_require=True
+        default=DATABASE_URL,
+        conn_max_age=500,
+        ssl_require=True,
     )
 
     DATABASES["default"].update(db_from_env)
     DEBUG = False
+
+
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     STATICFILES_STORAGE = (
@@ -141,6 +169,22 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Projeto T14",
+    "DESCRIPTION": "Projeto Demos Sprint 5",
+    "VERSION": "0.0.1",
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -165,20 +209,3 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.Account"
-
-SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=14)
-}
-
-REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 5,
-    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"
-}
-
-SPECTACULAR_SETTINGS = {
-    "TITLE": "PROJETO T14",
-    "DESCRIPTION": "PROJETO DEMO SPRINT 5",
-    "VERSION": "0.0.1"
-}
